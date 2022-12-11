fc5df9abf986c88b3acaeafcc8e115bd97e66a0a
yangcxPusto <619980817@qq.com>
2022-12-11 00:41:02 +0000

    init with an idle character

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cantonese-flashcard
-Display flashcards with Chinese characters and Jyutping with your comfortable speed
+
+Display flashcards with Chinese characters and Jyutping with your comfortable speed.
+
+This learning method is inspired by [Tshet Uinh Flashcard](https://github.com/nk2028/tshet-uinh-flashcard).
+
+# References
+
+Char sets:
+
+- [常用字字形表 (二零零零年修訂本) from rime-aca (4759 chars)](https://github.com/rime-aca/character_set/blob/master/%E5%B8%B8%E7%94%A8%E5%AD%97%E5%AD%97%E5%BD%A2%E8%A1%A8.txt)
+- [香港中文常用字 (東網一年節錄) - 表一 from hfhchan (1128 chars)](https://gist.github.com/hfhchan/98cee66dda2105ba177c2ac33d959bd3)
+- [香港中文常用字 (東網一年節錄) - 表二 from hfhchan (1008 chars)](https://gist.github.com/hfhchan/87ef6afd5dfc1abf806b2803855a8034)
---
+++ src/App.js
@@ -0,0 +1,124 @@
+import "purecss/build/pure.css";
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
+import FlashCard from "./components/FlashCard";
+import { 
+  Button, Container, CssBaseline, FormControl, 
+  InputLabel, MenuItem, Select, Stack, Typography
+} from '@mui/material';
+
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function App() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
+  const [lang, setLang] = React.useState('en');
+
+  const handleLangChange = (event) => {
+    setLang(event.target.value);
+  };
+
+  const getLocaleText = (i18nText, language) => {
+    return language in i18nText? i18nText[language] : i18nText["en"];
+  };
+
+  return (
+    <div>
+      
+      <Container maxWidth="sm">
+      <Stack spacing={4} px={2} pb={4}>
+        <Stack direction="row" spacing={2} justifyContent="flex-end" sx={{p: 3}}>
+          <Button variant="outlined" 
+                onClick={colorMode.toggleColorMode}
+                color="inherit"
+                sx={{textTransform: "none"}}
+                startIcon={theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}>
+            {getLocaleText(
+              {"en": "Theme", "zh-tra": "主題", "zh-sim": "主题", "tto-bro": "Tvo2D8ae", "tto": "VvaH"}, 
+              lang
+              )}
+          </Button>
+
+          <FormControl >
+            <InputLabel id="demo-simple-select-label">{getLocaleText(
+              {"en": "Language", "zh-tra": "語言", "zh-sim": "语言", "tto-bro": "Zei2ZeiH", "tto": "SRHM"}, 
+              lang
+              )}</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={lang}
+              label="Language"
+              onChange={handleLangChange}
+            >
+              <MenuItem value={"en"}>English</MenuItem>
+              <MenuItem value={"zh-tra"}>繁體中文</MenuItem>
+              <MenuItem value={"zh-sim"}>简体中文</MenuItem>
+              <MenuItem value={"tto-bro"}>b8Q7Z2D.</MenuItem>
+              <MenuItem value={"tto"}>mim</MenuItem>
+            </Select>
+          </FormControl>
+
+        </Stack>
+
+        <Typography>
+            Display flashcards with Chinese characters and Jyutping with your comfortable speed.
+        </Typography>
+
+        <FlashCard char={'字'} roma={'zi6'}></FlashCard>
+
+
+        {/* Search Module */}
+        {/* <TextField defaultValue="វិទ្យាស្ថានខុងជឺនៃរាជបណ្ឌិត្យសភាកម្ពុជា" id="input" onChange={(v) => setSentence(v.target.value)}
+          multiline
+          minRows={2} 
+          maxRows={Infinity} />
+        <Stack direction="row" justifyContent="flex-end">
+          <Button variant="outlined" onClick={() => handleClick()} sx={{width: "auto"}}>Lookup</Button>
+        </Stack> */}
+
+
+        {/* Speed Changing Module */}
+
+
+      </Stack>
+      </Container>
+    </div>
+  );
+}
+
+export default function AppWithColorToggler() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
